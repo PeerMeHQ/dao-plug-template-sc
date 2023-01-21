fc579dd30f43a231315aa32bab56b8622fd255d6
micha vie <micha@freeiam.com>
2023-01-21 23:59:19 +0100

    add views

--- src/lib.rs
+++ src/lib.rs
@@ -6,4 +6,18 @@ multiversx_sc::imports!();
 pub trait Plug {
     #[init]
     fn init(&self) {}
+
+    #[view(getDaoVoteWeight)]
+    fn get_dao_vote_weight_view(&self, address: ManagedAddress) -> BigUint {
+
+        // Implement your logic for user's vote weight here
+        BigUint::from(100u64)
+    }
+
+    #[view(getDaoMembers)]
+    fn get_dao_members_view(&self) -> MultiValueEncoded<ManagedAddress> {
+
+        // Return a list of DAO members' addresses here
+        MultiValueEncoded::new()
+    }
 }
