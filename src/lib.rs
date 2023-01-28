4e7c49f8b083f23c53326a772bc4c3b377bbef54
micha vie <micha@freeiam.com>
2023-01-29 00:35:34 +0100

    add example implementation

--- src/lib.rs
+++ src/lib.rs
@@ -9,15 +9,23 @@ pub trait Plug {
 
     #[view(getDaoVoteWeight)]
     fn get_dao_vote_weight_view(&self, address: ManagedAddress) -> BigUint {
-
         // Implement your logic for user's vote weight here
-        BigUint::from(100u64)
+        self.members().get(&address).unwrap_or_default()
     }
 
     #[view(getDaoMembers)]
     fn get_dao_members_view(&self) -> MultiValueEncoded<ManagedAddress> {
-
         // Return a list of DAO members' addresses here
-        MultiValueEncoded::new()
+        self.members().keys().collect()
     }
+
+    // 👇 The below is just for testing purposes
+
+    #[endpoint(addMember)]
+    fn add_member_endpoint(&self, address: ManagedAddress, weight: BigUint) {
+        self.members().insert(address, weight);
+    }
+
+    #[storage_mapper("members")]
+    fn members(&self) -> MapMapper<ManagedAddress, BigUint>;
 }
