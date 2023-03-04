4a87944b3580b1913a73e6573927dbb760a99c91
micha vie <micha@freeiam.com>
2023-03-04 17:21:15 +0100

    add optional gov token to view signatures

--- src/lib.rs
+++ src/lib.rs
@@ -8,31 +8,22 @@ pub trait Plug {
     fn init(&self) {}
 
     #[view(getDaoVoteWeight)]
-    fn get_dao_vote_weight_view(&self, address: ManagedAddress) -> BigUint {
+    fn get_dao_vote_weight_view(
+        &self,
+        address: ManagedAddress,
+        token: OptionalValue<TokenIdentifier>,
+    ) -> BigUint {
         // Implement your logic for user's vote weight here
-        self.members().get(&address).unwrap_or_default()
+        BigUint::from(100u64)
     }
 
     #[view(getDaoMembers)]
-    fn get_dao_members_view(&self) -> MultiValueEncoded<MultiValue2<ManagedAddress, BigUint>> {
+    fn get_dao_members_view(
+        &self,
+        token: OptionalValue<TokenIdentifier>,
+    ) -> MultiValueEncoded<MultiValue2<ManagedAddress, BigUint>> {
         // Return a list of DAO members' addresses + their vote weight here
 
-        let mut members_multi = MultiValueEncoded::new();
-
-        for (address, weight) in self.members().iter() {
-            members_multi.push((address, weight).into());
-        }
-
-        members_multi.into()
+        MultiValueEncoded::new()
     }
-
-    // 👇 The below is just for testing purposes
-
-    #[endpoint(addMember)]
-    fn add_member_endpoint(&self, address: ManagedAddress, weight: BigUint) {
-        self.members().insert(address, weight);
-    }
-
-    #[storage_mapper("members")]
-    fn members(&self) -> MapMapper<ManagedAddress, BigUint>;
 }
--- wasm/src/lib.rs
+++ wasm/src/lib.rs
@@ -5,9 +5,9 @@
 ////////////////////////////////////////////////////
 
 // Init:                                 1
-// Endpoints:                            3
+// Endpoints:                            2
 // Async Callback (empty):               1
-// Total number of exported functions:   5
+// Total number of exported functions:   4
 
 #![no_std]
 #![feature(alloc_error_handler, lang_items)]
@@ -20,7 +20,6 @@ multiversx_sc_wasm_adapter::endpoints! {
     (
         getDaoVoteWeight
         getDaoMembers
-        addMember
     )
 }
 
